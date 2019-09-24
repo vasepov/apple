0bfc9cc4e180872dd91f995b3ea8c0c40f017f1e
vasepov <1982>
2019-09-24 23:01:54 +0500

    Прикручен bower-asseе/bootbox

--- frontend/web/js/script.js
+++ frontend/web/js/script.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
     $('.eat-apple').click(function () {
         let id = $(this).attr('data-id'), size = $('.slider-eat-' + id).val();
         $.post('eat-apple', {id: id, size: size}, function (data) {
-            alert(data.message);
+            bootbox.alert(data.message);
             if (data.result) {
                 if (size == 100) {
                     $('.tr-apple-' + id).remove();
